a3df4588537da8398fc99993d1a39a3436940b12
koobirdza <koobirdza@gmail.com>
2026-03-05 16:42:39 +0700

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-// PWA Service Worker (Option B: No-cache + Auto-update)
-self.addEventListener("install", () => self.skipWaiting());
-self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
-self.addEventListener("fetch", (event) => {
-  event.respondWith(fetch(event.request, { cache: "no-store" }));
-});
