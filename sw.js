c2d40c258e4a96e1c55ab916cd5fb05d9e728ff4
koobirdza <koobirdza@gmail.com>
2026-03-11 03:22:37 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,2 @@
-const VERSION = "v30-refactor-disabled";
-self.addEventListener("install", (event) => {
-  self.skipWaiting();
-});
-self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim());
-});
-self.addEventListener("fetch", () => {
-  // intentionally no caching during stabilization period
-});
+self.addEventListener('install', ()=> self.skipWaiting());
+self.addEventListener('activate', (event)=> event.waitUntil(self.clients.claim()));
