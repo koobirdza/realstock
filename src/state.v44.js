46aabe34426abed9ac9b5bec8e7b7fedec2dec27
koobirdza <koobirdza@gmail.com>
2026-03-11 04:10:25 +0700

    Add files via upload

---
+++ src/state.v44.js
@@ -0,0 +1,1 @@
+export const state={employee:null,mode:null,path:[],destination:"",catalogs:{},orderRows:[],stockSummary:{}};export function resetNavigation(){state.path=[];state.destination="";}export function setEmployee(name){state.employee=name||null;}export function setMode(mode){state.mode=mode||null;resetNavigation();}
