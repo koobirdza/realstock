bbe2e90062761e2923554afdbeb7d47b521b96f6
koobirdza <koobirdza@gmail.com>
2026-03-12 06:14:50 +0700

    Update and rename utils.v46.js to utils.v47.js

---
+++ src/utils.v47.js
@@ -0,0 +1,24 @@
+export function qs(id) {
+  return document.getElementById(id);
+}
+
+export function escapeHtml(value) {
+  return String(value ?? "")
+    .replaceAll("&", "&amp;")
+    .replaceAll("<", "&lt;")
+    .replaceAll(">", "&gt;")
+    .replaceAll('"', "&quot;")
+    .replaceAll("'", "&#39;");
+}
+
+export function debounce(fn, wait = 250) {
+  let t = null;
+  return (...args) => {
+    clearTimeout(t);
+    t = setTimeout(() => fn(...args), wait);
+  };
+}
+
+export function createRequestId() {
+  return "req_" + Date.now() + "_" + Math.random().toString(36).slice(2, 10);
+}
