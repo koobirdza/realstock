7b1db070e986918ceb3f8f2acce8432ccfb2c599
koobirdza <koobirdza@gmail.com>
2026-03-12 16:12:31 +0700

    Update config.v49.js

--- src/config.v49.js
+++ src/config.v49.js
@@ -1,5 +1,5 @@
 export const APP_VERSION = "v49.2-instant-nav";
-export const GOOGLE_SCRIPT_URL = "PUT_YOUR_WEB_APP_EXEC_URL_HERE";
+export const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwSOEIId8C7BVTxCYwiJoSG7N7k4OSy1TfZXJyDHSD2AWSJ7NWGboHYIqMtqbqP2ISn4Q/exec";
 export const ENABLE_SERVICE_WORKER = true;
 export const CACHE_TTL = {
   catalog: 10 * 60 * 1000,
